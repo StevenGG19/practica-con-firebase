1cd8a53e71764ee82989dc840623428fd03372e7
StevenGG19 <stevengg39@gmail.com>
2021-06-03 21:06:19 -0500

    actualizacion

--- app/src/main/java/com/platzi/android/firestore/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/platzi/android/firestore/ui/activity/LoginActivity.kt
@@ -2,12 +2,19 @@ package com.platzi.android.firestore.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.platzi.android.firestore.R
 import com.platzi.android.firestore.databinding.ActivityLoginBinding
+import com.platzi.android.firestore.model.User
+import com.platzi.android.firestore.network.Callback
+import com.platzi.android.firestore.network.FirestoreService
+import com.platzi.android.firestore.network.USERS_COLLECTION_NAME
+import java.lang.Exception
 
 const val USERNAME_KEY = "username_key"
 
@@ -15,24 +22,46 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var bin: ActivityLoginBinding
     private val TAG = "LoginActivity"
     private var auth = FirebaseAuth.getInstance()
+    private lateinit var firestoreService: FirestoreService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bin = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(bin.root)
+
+        firestoreService = FirestoreService(FirebaseFirestore.getInstance())
     }
 
 
     fun onStartClicked(view: View) {
+        view.isEnabled = false
         auth.signInAnonymously()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val username = bin.username.text.toString()
-                    startMainActivity(username)
+                    val user = User()
+                    user.userName = username
+                    saveUserAndStartActivity(user, view)
                 } else {
                     showErrorMessage(view)
+                    view.isEnabled = true
                 }
             }
+    }
+
+    private fun saveUserAndStartActivity(user: User, view: View) {
+        firestoreService.setDocument(user, USERS_COLLECTION_NAME, user.userName, object : Callback<Void>{
+            override fun onSuccess(result: Void?) {
+                startMainActivity(user.userName)
+            }
+
+            override fun onFailed(exception: Exception) {
+                showErrorMessage(view)
+                Log.e(TAG, "error", exception)
+                view.isEnabled = true
+            }
+
+        })
     }
 
     private fun showErrorMessage(view: View) {
